8200a44dc7cf8655c8165c843f684360f8f97bc8
Celdredge <haydendhill01@gmail.com>
2026-03-29 14:17:03 -0600

    Added more defensive programming against type errors in the User Model and Schema.

--- app/models/user.py
+++ app/models/user.py
@@ -1,5 +1,8 @@
 from ..database import db
 from werkzeug.security import generate_password_hash, check_password_hash
+from datetime import datetime, timezone
+from typing import Tuple
+from math import isfinite
 
 #Defines in Python an abstraction for a User table. This is the model that SQLAlchemy will use to create the database and interact with it.
 class User(db.Model):
@@ -20,35 +23,71 @@ class User(db.Model):
 
         super().__init__(**kwargs)
 
-
+    #Note: last_seen needs to use lambdas because datetime.now is evaluated once at runtime, not per instance. Using a lambda ensures that a new
+    #timestamp is generated each time a User instance is created or updated, rather than all instances sharing the same timestamp.
     id =db.Column(db.Integer, primary_key=True)
     badge_num = db.Column(db.String(50), unique=True, nullable=False)
     name = db.Column(db.String(100), nullable=False)
     password_hash = db.Column(db.String(128), nullable=False)
     admin = db.Column(db.Boolean, default=False)
+    latitude = db.Column(db.Float, nullable=True)
+    longitude = db.Column(db.Float, nullable=True)
+    last_seen = db.Column(db.DateTime, default=lambda:datetime.now(timezone.utc), onupdate=lambda:datetime.now(timezone.utc))
+    status = db.Column(db.String(20), default="Offline", nullable=False)
 
-    def set_password(self,password):
+    #Unchanging Checks
+    APPROVED_STATUS = ["Online","Offline","Away"]
+
+    #User Attributes
+    def set_password(self,password) -> Tuple[bool,str | None]:
         """Encrypts using werkzeug.security's generate_password_hash function and stores the hash in the password_hash field.
 
         Args:
             password (str): The plaintext password to be hashed and stored.
+        
+        Returns:
+            Tuple[bool, str | None]: A tuple where the first element is a boolean indicating
+            success (True if the password was set successfully, False otherwise), and the second element is an optional error message
+             (None if the password was set successfully, or a string describing the error if it was not).
         """
+
+        if self.is_string(password) == False:
+            return (False,"Password must be a non-empty string.")
+        
+        if len(password) < 8 or len(password) > 128:
+            return (False,"Password must be between 8 and 128 characters long.")
+
         self.password_hash = generate_password_hash(password)
+        return (True,None)
     
-    def check_password(self,password):
+    def check_password(self,password) -> Tuple[bool,str | None]:
         """Checks if the provided password matches the stored password hash using werkzeug.security's check_password_hash function.
 
         Args:
             password (str): The password to be checked against the stored password hash.
         
         Returns:
-            bool: True if the password matches the stored hash, False otherwise.
+            Tuple[bool, str | None]: A tuple where the first element is a boolean indicating 
+            success (True if the password matches the hash, False otherwise), and the second element is an optional error message 
+            (None if the password matches, or a string describing the error if it does not).
         
         """
+
+        #Type
+        if self.is_string(password) == False:
+            return (False,"Password must be a non-empty string.")
+
+        #Length
+        if len(password) < 8 or len(password) > 128:
+            return (False,"Password must be between 8 and 128 characters long.")
         
-        return check_password_hash(self.password_hash,password)
+        #Actual Check
+        if check_password_hash(self.password_hash,password):
+            return (True,None)
+        else:
+            return (False,"Incorrect password.")
     
-    def to_dict(self):
+    def to_dict(self) ->dict:
         """Converts the User object into a dictionary format.
 
         Returns:
@@ -58,14 +97,114 @@ class User(db.Model):
         return {
             "id": self.id,
             "badge_num": self.badge_num,
-            "name": self.name
+            "name": self.name,
+            "latitude": self.latitude,
+            "longitude": self.longitude,
+            "last_seen": self.last_seen.isoformat(),
+            "status": self.status,
+            "admin": self.admin
         }
     
-    def set_admin(self):
+    def set_admin(self) -> None:
         """Toggles the admin status of the user.
             Needed because we cannot access attributes outside of the class
             without assigning all to "self".
         """
+        if self.admin == None:
+            self.admin = False
+        
         if self.admin == False: 
             self.admin = True 
-        else: self.admin = False
+        else: 
+            self.admin = False
+
+    #User State
+    def update_status(self,new_status:str) -> Tuple[bool,str | None]:
+        """Updates the user's status to one of the approved statuses.
+
+        Args:
+            new_status (str): The new status to be set for the user. Must be one of "Online", "Offline", or "Away".
+
+        Returns:
+            Tuple[bool, str | None]: A tuple where the first element is a boolean indicating success (True if the status was updated successfully, False otherwise), 
+            and the second element is an optional error message (None if the update was successful, or a string describing the error if it was not).
+        """
+        if new_status not in self.APPROVED_STATUS or not self.is_string(new_status):
+            return (False,f"Status must be one of {self.APPROVED_STATUS}")
+        
+        self.status = new_status
+        return (True,None)
+    
+    def set_location(self,latitude:float,longitude:float) -> Tuple[bool,str | None]:
+        """Sets the user's location by updating the latitude and longitude fields.
+
+        Args:
+            latitude (float): The latitude value to be set for the user's location. Must be between -90 and 90.
+            longitude (float): The longitude value to be set for the user's location. Must be between -180 and 180.
+
+        Returns:
+            Tuple[bool, str | None]: A tuple where the first element is a boolean indicating success (True if the location was updated successfully, False otherwise), 
+            and the second element is an optional error message (None if the update was successful, or a string describing the error if it was not).
+        """
+
+        #Type
+        if self.is_float(latitude) == False or self.is_float(longitude) == False:
+            return (False,"Latitude and Longitude must be decimals.")
+        
+        #Range
+        if latitude < -90 or latitude > 90:
+            return (False,"Latitude must be between -90 and 90.")
+        
+        if longitude < -180 or longitude > 180:
+            return (False,"Longitude must be between -180 and 180.")
+        
+        self.latitude = latitude
+        self.longitude = longitude
+        return (True,None)
+
+
+
+    #Helper Methods
+    def is_string(self,value) -> bool:
+        """Checks if the provided value is a string.
+
+        Args:
+            value: The value to be checked.
+        
+        Returns:
+            bool: True if the value is a string, False otherwise.
+        """
+    
+        if not isinstance(value,str) or value.strip() == "":
+            return False
+    
+        return True
+
+    def is_int(self,value) -> bool:
+        """Checks if the provided value is an integer.
+
+        Args:
+            value: The value to be checked.
+        
+        Returns:
+            bool: True if the value is an integer, False otherwise.
+        """
+
+        if not isinstance(value,int):
+            return False
+        
+        return True
+    
+    def is_float(self,value) -> bool:
+        """Checks if the provided value is a float.
+
+        Args:
+            value: The value to be checked.
+        
+        Returns:
+            bool: True if the value is a float, False otherwise.
+        """
+        if not isinstance(value,float) or not isfinite(value):
+            return False
+        
+        return True
--- app/schemas/user.py
+++ app/schemas/user.py
@@ -12,3 +12,7 @@ class UserSchema(Schema):
     badge_num = fields.Str(required=True, validate=validate.Regexp(r'^SHD-[A-Z]{2}[0-9]{4}$'))
     name = fields.Str(required=True, validate=validate.Regexp(r'^\w+ \w+$'))
     password = fields.Str(required=True, validate=validate.Length(min=8,max=128))
+    status = fields.Str(validate=validate.OneOf(["Online","Offline","Away"]))
+    latitude = fields.Float(validate=validate.Range(min=-90, max=90))
+    longitude = fields.Float(validate=validate.Range(min=-180, max=180))
+    
